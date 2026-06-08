a5461c1da2304a2077adb0e04498ee5dce72c8ec
Samiksha <jainmistii110705@gmail.com>
2026-06-08 19:34:30 +0530

    Python-S3-RDS

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    SECRET_KEY = "AKIA4DSA4NSFJMGZR4OW"
+
+    MYSQL_HOST = "database-1.c2f6wqc0iacj.us-east-1.rds.amazonaws.com"
+    MYSQL_USER = "admin"
+    MYSQL_PASSWORD = "Sam8484123"
+    MYSQL_DB = "flask_auth"
+
+    # AWS S3
+    AWS_ACCESS_KEY_ID = "AKIA4DSA4NSFJMGZR4OW"
+    AWS_SECRET_ACCESS_KEY = "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"
+    AWS_BUCKET_NAME = "amzn-sam34556-project-bucket"
+    AWS_REGION = "us-east-1"
